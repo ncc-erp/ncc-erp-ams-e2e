7417ee20b93008bf9b4b44c14ec59147a58115c0
Hoàng Hà Chi <chi.hoangha@ncc.asia>
2024-05-13 10:48:52 +0700

    hoanghachi

--- src/pageObjects/CreateAsset.ts
+++ src/pageObjects/CreateAsset.ts
@@ -4,128 +4,129 @@ import { waitElementVisible } from "@/features/verrify/waitElemVisible";
 import { verifyText } from "@/features/verrify/verifyText";
 import { HTMLElement } from "./components/controls/HtmlElement";
 class createNewAsset extends BasePage {
-constructor() {
-super('/login')
-}
-// elements
-get thietBiMenu() {
-return HTMLElement.byXpath("//span[text() = 'Thiết bị']");
-}
-get tatCaSubMenu() {
-return HTMLElement.byXpath("//*[@data-test-id='sub-menu-item' and text()='Tất cả']");
-}
-get themMoiButton() {
-return HTMLElement.byXpath("//span[@class = 'ant-page-header-heading-extra']//span[text() = 'Thêm mới']")
-}
-get taoTaiSanModal() {
-return HTMLElement.byXpath("//div[@class = 'ant-modal-content']")
-}
-get theTaiSanField() {
-return HTMLElement.byXpath("//input[@type = 'text' and @id='asset_tag']")
-}
-get soSeRiField() {
-return HTMLElement.byXpath("//input[@type = 'text' and @id='serial']")
-}
-get kieuTaiSanField() {
-return HTMLElement.byXpath("//input[@type = 'search' and @id='model']")
-}
-get kieuTaiSanOption() {
-return HTMLElement.byXpath("//div[text() = 'Headphone - DN Headphone - DN']")
-}
-get vanPhongField() {
-return HTMLElement.byXpath("//input[@type = 'search' and @id='rtd_location']")    }
-get vanPhongOption() {
-return HTMLElement.byXpath("//div[@title = 'NCC DN']")
-}
-get baoHanhField() {
-return HTMLElement.byXpath("//input[@type = 'number' and @id='warranty_months']")
-}
-get trangThaiField() {
-return HTMLElement.byXpath("//input[@type = 'search' and @id='status_label']")
-}
-get trangThaiOption() {
-return HTMLElement.byXpath("//div[@title='Ready to Deploy']")
-}
-get tenTaiSanField() {
-return HTMLElement.byXpath("//input[@type = 'text' and @id='name' and @placeholder='Tên tài sản']")
-}
-get nhaCungCapField() {
-return HTMLElement.byXpath("//input[@type = 'search' and @id='supplier']")
-}
-get nhaCungCapOption() {
-return HTMLElement.byXpath("//div[@title= 'Tiki']")
-}
-get maDonHangField() {
-return HTMLElement.byXpath("//input[@type = 'text' and @id='order_number']")
-}
-get chiPhiMuaHangField() {
-return HTMLElement.byXpath("//input[@type = 'number' and @id='purchase_cost']")
-}
-get themMoiButtonSubmit() {
-return HTMLElement.byXpath("//button[@type='submit']//span[text() = 'Thêm mới']")
-}
-get toastSuccess() {
-return HTMLElement.byXpath("//div[@class='ant-notification-notice-description' and text() = 'Asset created successfully. :)']")
-}
-// actions
-async clickToThietBiMenu() {
-await this.thietBiMenu.click();
-}
-async clickToTatCaSubmenu() {
-await this.tatCaSubMenu.click();
-}
-async clickToThemMoiButton() {
-await this.themMoiButton.click()
-}
-async typeTheTaiSan(theTaiSan: string) {
-await this.theTaiSanField.type(theTaiSan)
-}
-async typeSoSeRi(soSeRi: string) {
-await this.soSeRiField.type(soSeRi)
-}
-async clickToKieuTaiSanField() {
-await this.kieuTaiSanField.click()
-}
-async selectKieuTaiSan() {
-await this.kieuTaiSanOption.click()
-}
-async clickToVanPhongField() {
-await this.vanPhongField.click()
-}
-async selectVanPhong() {
-await this.vanPhongOption.click()
-}
-async typeBaoHanh(bHnumber: string) {
-await this.baoHanhField.type(bHnumber)
-}
-async clickToTrangThaiField() {
-await this.trangThaiField.click()
-}
-async selectTrangThai() {
-await this.trangThaiOption.click()
-}
-async typeTenTaiSan(tenTaiSan: string) {
-await this.tenTaiSanField.type(tenTaiSan)
-}
-async clickToNhaCungCapField() {
-await this.nhaCungCapField.click()
-}
-async selectNhaCungCap() {
-await this.nhaCungCapOption.click()
-}
-async typeMaDonHang(mdh: string) {
-await this.maDonHangField.type(mdh)
-}
-async typeChiPhiMuaHang(cost: string) {
-await this.chiPhiMuaHangField.type(cost)
-}
-async clickToThemMoiButtonSubmit() {
-await this.themMoiButtonSubmit.click()
-}
-async verifyToastSuccess(toast: string) {
-waitElementVisible(this.toastSuccess, toast)
-verifyText(this.toastSuccess, toast)
-}
+    constructor() {
+        super('/login')
+    }
+    // elements
+    get thietBiMenu() {
+        return HTMLElement.byXpath("//span[text() = 'Thiết bị']");
+    }
+    get tatCaSubMenu() {
+        return HTMLElement.byXpath("//*[@data-test-id='sub-menu-item' and text()='Tất cả']");
+    }
+    get themMoiButton() {
+        return HTMLElement.byXpath("//span[@class = 'ant-page-header-heading-extra']//span[text() = 'Thêm mới']")
+    }
+    get taoTaiSanModal() {
+        return HTMLElement.byXpath("//div[@class = 'ant-modal-content']")
+    }
+    get theTaiSanField() {
+        return HTMLElement.byXpath("//input[@type = 'text' and @id='asset_tag']")
+    }
+    get soSeRiField() {
+        return HTMLElement.byXpath("//input[@type = 'text' and @id='serial']")
+    }
+    get kieuTaiSanField() {
+        return HTMLElement.byXpath("//input[@type = 'search' and @id='model']")
+    }
+    get kieuTaiSanOption() {
+        return HTMLElement.byXpath("//div[text() = 'Headphone - DN Headphone - DN']")
+    }
+    get vanPhongField() {
+        return HTMLElement.byXpath("//input[@type = 'search' and @id='rtd_location']")
+    }
+    get vanPhongOption() {
+        return HTMLElement.byXpath("//div[@title = 'NCC DN']")
+    }
+    get baoHanhField() {
+        return HTMLElement.byXpath("//input[@type = 'number' and @id='warranty_months']")
+    }
+    get trangThaiField() {
+        return HTMLElement.byXpath("//input[@type = 'search' and @id='status_label']")
+    }
+    get trangThaiOption() {
+        return HTMLElement.byXpath("//div[@title='Ready to Deploy']")
+    }
+    get tenTaiSanField() {
+        return HTMLElement.byXpath("//input[@type = 'text' and @id='name' and @placeholder='Tên tài sản']")
+    }
+    get nhaCungCapField() {
+        return HTMLElement.byXpath("//input[@type = 'search' and @id='supplier']")
+    }
+    get nhaCungCapOption() {
+        return HTMLElement.byXpath("//div[@title= 'Tiki']")
+    }
+    get maDonHangField() {
+        return HTMLElement.byXpath("//input[@type = 'text' and @id='order_number']")
+    }
+    get chiPhiMuaHangField() {
+        return HTMLElement.byXpath("//input[@type = 'number' and @id='purchase_cost']")
+    }
+    get themMoiButtonSubmit() {
+        return HTMLElement.byXpath("//button[@type='submit']//span[text() = 'Thêm mới']")
+    }
+    get toastSuccess() {
+        return HTMLElement.byXpath("//div[@class='ant-notification-notice-description' and text() = 'Asset created successfully. :)']")
+    }
+    // actions
+    async clickToThietBiMenu() {
+        await this.thietBiMenu.click();
+    }
+    async clickToTatCaSubmenu() {
+        await this.tatCaSubMenu.click();
+    }
+    async clickToThemMoiButton() {
+        await this.themMoiButton.click()
+    }
+    async typeTheTaiSan(theTaiSan: string) {
+        await this.theTaiSanField.type(theTaiSan)
+    }
+    async typeSoSeRi(soSeRi: string) {
+        await this.soSeRiField.type(soSeRi)
+    }
+    async clickToKieuTaiSanField() {
+        await this.kieuTaiSanField.click()
+    }
+    async selectKieuTaiSan() {
+        await this.kieuTaiSanOption.click()
+    }
+    async clickToVanPhongField() {
+        await this.vanPhongField.click()
+    }
+    async selectVanPhong() {
+        await this.vanPhongOption.click()
+    }
+    async typeBaoHanh(bHnumber: string) {
+        await this.baoHanhField.type(bHnumber)
+    }
+    async clickToTrangThaiField() {
+        await this.trangThaiField.click()
+    }
+    async selectTrangThai() {
+        await this.trangThaiOption.click()
+    }
+    async typeTenTaiSan(tenTaiSan: string) {
+        await this.tenTaiSanField.type(tenTaiSan)
+    }
+    async clickToNhaCungCapField() {
+        await this.nhaCungCapField.click()
+    }
+    async selectNhaCungCap() {
+        await this.nhaCungCapOption.click()
+    }
+    async typeMaDonHang(mdh: string) {
+        await this.maDonHangField.type(mdh)
+    }
+    async typeChiPhiMuaHang(cost: string) {
+        await this.chiPhiMuaHangField.type(cost)
+    }
+    async clickToThemMoiButtonSubmit() {
+        await this.themMoiButtonSubmit.click()
+    }
+    async verifyToastSuccess(toast: string) {
+        waitElementVisible(this.toastSuccess, toast)
+        verifyText(this.toastSuccess, toast)
+    }
 }
 export default new createNewAsset();
 
